3887db39061afc907176d7ab4b50032a57d8f937
Max Weidemann <max.weidemann499@gmail.com>
2019-06-11 14:06:12 +0200

    Test java call through pyhon module.

---
+++ citlab_python_util/external/java/tmp.py
@@ -0,0 +1,12 @@
+import jpype
+
+
+def get_java_util_object():
+    return jpype.JPackage("citlab_python_util.external.java").Util()
+
+
+if __name__ == '__main__':
+    jpype.startJVM(jpype.getDefaultJVMPath())
+    java_util_obj = get_java_util_object()
+    java_util_obj.testPrint()
+    jpype.shutdownJVM()
