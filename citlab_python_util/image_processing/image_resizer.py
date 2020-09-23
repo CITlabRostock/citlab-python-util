e5b3399c4137b4f43ac1f597e6a50f32ea2038e2
Johannes Michael <johannesmichael1993@gmail.com>
2020-09-23 13:32:36 +0200

    added various utility functions

--- citlab_python_util/image_processing/image_resizer.py
+++ citlab_python_util/image_processing/image_resizer.py
@@ -2,7 +2,9 @@ import os
 
 import cv2
 import numpy as np
+import tensorflow as tf
 
+from citlab_python_util.basic import shape_utils
 from citlab_python_util.io.file_loader import load_image
 from citlab_python_util.io.file_loader import load_list_file
 from citlab_python_util.logging import custom_logging
@@ -68,6 +70,157 @@ class ImageResizer:
             return cv2.resize(image, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_CUBIC)
         else:
             return image
+
+
+def resize_image_fixed(tensor_dict,
+                       new_height=600,
+                       new_width=1024,
+                       method=tf.image.ResizeMethod.BILINEAR,
+                       align_corners=False):
+    """Resizes images to the given height and width and adds the new image shapes to the tensor dict.
+
+    Args:
+      tensor_dict:
+      new_height: (optional) (scalar) desired height of the image.
+      new_width: (optional) (scalar) desired width of the image.
+      method: (optional) interpolation method used in resizing. Defaults to
+              BILINEAR.
+      align_corners: bool. If true, exactly align all 4 corners of the input
+                     and output. Defaults to False.
+
+    Returns:
+      Note that the position of the resized_image_shape changes based on whether
+      masks are present.
+      resized_image: A tensor of size [new_height, new_width, channels].
+      resized_masks: If masks is not None, also outputs masks. A 3D tensor of
+        shape [num_instances, new_height, new_width]
+      resized_image_shape: A 1D tensor of shape [3] containing the shape of the
+        resized image.
+    """
+    with tf.name_scope(
+            'ResizeImage',
+            values=[tensor_dict['image'], new_height, new_width, method, align_corners]):
+        tensor_dict['image'] = tf.image.resize(
+            tensor_dict['image'], tf.stack([new_height, new_width]),
+            method=method,
+            align_corners=align_corners)
+        tensor_dict['image_shape'] = shape_utils.combined_static_and_dynamic_shape(tensor_dict['image'])
+        return tensor_dict
+
+
+def resize_image_ratio(tensor_dict,
+                       min_dimension=600,
+                       max_dimension=1024,
+                       method=tf.image.ResizeMethod.BILINEAR,
+                       align_corners=False,
+                       pad_to_max_dimension=False,
+                       per_channel_pad_value=(0, 0, 0)):
+    """Resizes an image so its dimensions are within the provided values.
+
+    Args:
+      tensor_dict:
+      min_dimension: (scalar) desired size of the smaller image
+                     dimension.
+      max_dimension: (scalar) maximum allowed size
+                     of the larger image dimension.
+      method: (optional) interpolation method used in resizing. Defaults to
+              BILINEAR.
+      align_corners: bool. If true, exactly align all 4 corners of the input
+                     and output. Defaults to False.
+      pad_to_max_dimension: Whether to resize the image and pad it with zeros
+        so the resulting image is of the spatial size
+        [max_dimension, max_dimension]. If masks are included they are padded
+        similarly.
+      per_channel_pad_value: A tuple of per-channel scalar value to use for
+        padding. By default pads zeros.
+
+    Returns:
+        tensor_dict
+
+    """
+    with tf.name_scope('ResizeToRange', values=[tensor_dict['image'], min_dimension]):
+        image = tensor_dict['image']
+        if image.get_shape().is_fully_defined():
+            new_size = _compute_new_static_size(image, min_dimension, max_dimension)
+        else:
+            new_size = _compute_new_dynamic_size(image, min_dimension, max_dimension)
+        tensor_dict['image_shape'] = new_size
+        new_image = tf.image.resize(
+            image, new_size[:-1], method=method, align_corners=align_corners)
+        tensor_dict['image'] = new_image
+        if pad_to_max_dimension:
+            channels = tf.unstack(new_image, axis=2)
+            if len(channels) > len(per_channel_pad_value):
+                raise ValueError('Number of channels must be less or equal to the length of '
+                                 'per-channel pad value.')
+            new_image = tf.stack(
+                [
+                    tf.pad(
+                        channels[i], [[0, max_dimension - new_size[0]],
+                                      [0, max_dimension - new_size[1]]],
+                        constant_values=per_channel_pad_value[i])
+                    for i in range(len(channels))
+                ],
+                axis=2)
+            new_image.set_shape([max_dimension, max_dimension, len(channels)])
+
+        return tensor_dict
+
+
+def _compute_new_static_size(image, min_dimension, max_dimension):
+    """Compute new static shape for resize_to_range method."""
+    image_shape = image.get_shape().as_list()
+    orig_height = image_shape[0]
+    orig_width = image_shape[1]
+    num_channels = image_shape[2]
+
+    # Scale factor such that maximal dimension is at most max_dimension
+    orig_max_dim = max(orig_height, orig_width)
+    small_scale_factor = max_dimension / float(orig_max_dim)
+    # if this factor is less than 1 we have to act!
+
+    # Scale factor such that minimal dimension is at least min_dimension
+    orig_min_dim = min(orig_height, orig_width)
+    large_scale_factor = min_dimension / float(orig_min_dim)
+    # If image is already big enough... do nothing
+    large_scale_factor = max(large_scale_factor, 1.0)
+
+    # Take the minimum (we ensure that maxdim is not exceeded and if possible min_dim is met also)
+    scale_factor = min(small_scale_factor, large_scale_factor)
+
+    new_height = int(round(orig_height * scale_factor))
+    new_width = int(round(orig_width * scale_factor))
+    new_size = [new_height, new_width]
+    return tf.constant(new_size + [num_channels])
+
+
+def _compute_new_dynamic_size(image, min_dimension, max_dimension):
+    """Compute new dynamic shape for resize_to_range method."""
+    image_shape = tf.shape(image)
+    orig_height = tf.cast(image_shape[0], dtype=tf.float32)
+    orig_width = tf.cast(image_shape[1], dtype=tf.float32)
+    num_channels = image_shape[2]
+
+    # Scale factor such that maximal dimension is at most max_dimension
+    orig_max_dim = tf.maximum(orig_height, orig_width)
+    max_dimension = tf.constant(max_dimension, dtype=tf.float32)
+    small_scale_factor = max_dimension / orig_max_dim
+    # if this factor is less than 1 we have to act!
+
+    # Scale factor such that minimal dimension is at least min_dimension
+    orig_min_dim = tf.minimum(orig_height, orig_width)
+    min_dimension = tf.constant(min_dimension, dtype=tf.float32)
+    large_scale_factor = min_dimension / orig_min_dim
+    # If image is already big enough... do nothing
+    large_scale_factor = tf.maximum(large_scale_factor, 1.0)
+
+    # Take the minimum (we ensure that maxdim is not exceeded and if possible min_dim is met also)
+    scale_factor = tf.minimum(small_scale_factor, large_scale_factor)
+
+    new_height = tf.cast(tf.round(orig_height * scale_factor), dtype=tf.int32)
+    new_width = tf.cast(tf.round(orig_width * scale_factor), dtype=tf.int32)
+    new_size = tf.stack([new_height, new_width])
+    return tf.stack(tf.unstack(new_size) + [num_channels])
 
 
 if __name__ == '__main__':
